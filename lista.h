cb6f64b6e41a26884a429bb8856e6d467f0824b4
dmls <76413717+diogoo-lemos@users.noreply.github.com>
2023-05-04 11:36:20 +0100

    Update lista.h

--- lista.h
+++ lista.h
@@ -1,10 +1,13 @@
-#ifndef PROJETOPPP_LISTA_H
-#define PROJETOPPP_LISTA_H
+#ifndef PROJETOPPP_LISTAS_H
+#define PROJETOPPP_LISTAS_H
 
-#include "struct.h"
+#include "structs.h"
 
+void search_element()list_node **list, int key, list_node **prev, list_node **cur);
 void insert(list_node **list, char nome, char tipo, char time, int num);
 void remove_element(list_node **list, int key);
+void print_list(list_node *list);
 
 
-#endif //PROJETOPPP_LISTA_H
+#endif //PROJETOPPP_LISTAS_H
+
