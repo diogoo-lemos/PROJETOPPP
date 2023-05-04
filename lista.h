76a302ff9a3e579e2687338f08b1742d9aaaf215
dmls <76413717+diogoo-lemos@users.noreply.github.com>
2023-05-04 11:44:51 +0100

    Update lista.h

--- lista.h
+++ lista.h
@@ -4,7 +4,7 @@
 #include "struct.h"
 
 void search_element()list_node **list, int key, list_node **prev, list_node **cur);
-void insert(list_node **list, char nome, char tipo, char time, int num);
+void insert(list_node **list, char nome, char tipo, double min, double hour, double day, double month, int num);
 void remove_element(list_node **list, int key);
 void print_list(list_node *list);
 
